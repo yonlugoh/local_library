502171055e9491daa2ab2962d6dac5bdb3ffeaa0
DESKTOP-OV192TK\yonlu <ygoh019@e.ntu.edu.sg>
2018-07-30 17:02:39 +0800

    Update with github url

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,3 +1,6 @@
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+
 var Author = require('../models/author');
 var async = require('async')
 var Book = require('../models/book')
