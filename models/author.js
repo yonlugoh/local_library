85118bb6d862cbaf794ec75add601d2b2b6574ec
DESKTOP-OV192TK\yonlu <ygoh019@e.ntu.edu.sg>
2018-07-30 15:27:52 +0800

    Added my own code for genre and authors

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var moment = require('moment'); // For date handling.
 
 var Schema = mongoose.Schema;
 
@@ -24,6 +25,46 @@ AuthorSchema
 .get(function () {
   return '/catalog/author/' + this._id;
 });
+
+
+AuthorSchema
+.virtual('lifespan')
+.get(function () {
+  var lifetime_string='';
+  if (this.date_of_birth) {
+      lifetime_string=moment(this.date_of_birth).format('MMMM Do, YYYY');
+      }
+  lifetime_string+=' - ';
+  if (this.date_of_death) {
+      lifetime_string+=moment(this.date_of_death).format('MMMM Do, YYYY');
+      }
+  return lifetime_string;
+});
+
+AuthorSchema
+.virtual('date_of_birth_formatted')
+.get(function () {
+  if (this.date_of_birth){
+    return moment(this.date_of_birth).format('MMMM Do, YYYY');
+  }
+  else
+  {
+    return ' ';
+  }
+});
+
+AuthorSchema
+.virtual('date_of_death_formatted')
+.get(function () {
+  if (this.date_of_death){
+    return moment(this.date_of_death).format('MMMM Do, YYYY');
+  }
+  else
+  {
+    return ' ';
+  }
+});
+
 
 //Export model
 module.exports = mongoose.model('Author', AuthorSchema);
